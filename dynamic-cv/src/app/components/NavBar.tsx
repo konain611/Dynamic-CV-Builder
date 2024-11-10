6977256ef159ca9ddfc5cab646127878284b1029
Syed Konain Nasir <konain611@gmail.com>
2024-11-10 21:57:08 +0500

    Update NavBar.tsx

--- dynamic-cv/src/app/components/NavBar.tsx
+++ dynamic-cv/src/app/components/NavBar.tsx
@@ -1,8 +1,11 @@
+import Link from 'next/link'
+
 export default function NavBar() {
     return (
         <div>
             <nav className="navbar">
-                <link href="/">Home</link>
+
+                <Link href="/">Home</Link>
                 <a href="#personal-info">Personal Info</a>
                 <a href="#education">Education</a>
                 <a href="#experience">Experience</a>
